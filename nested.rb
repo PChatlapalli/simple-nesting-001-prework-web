a16cf507d38b83dfe6983bd7b2ea0a465ff3c641
PChatlapalli <pavan.chatlapalli@gmail.com>
2016-01-15 14:58:50 -0500

    Done.

--- nested.rb
+++ nested.rb
@@ -1,8 +1,8 @@
 
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
+	programmer_hash = {
+        :grace_hopper => 
+        {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +54,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -66,6 +69,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+        :yukihiro_matsumoto =>{
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,6 +86,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+
 
     
 end
@@ -91,7 +99,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -100,7 +108,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     #return programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
@@ -118,7 +126,7 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
 
